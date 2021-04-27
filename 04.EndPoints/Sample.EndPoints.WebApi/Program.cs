141ce8cfe1a9da176246ebc9c4194bc13c49ad9a
fatemeh bahagir <fatemeh.bahagir1377@gmail.com>
2021-04-27 11:14:13 +0430

    Create a Sample Project with all layers

---
+++ 04.EndPoints/Sample.EndPoints.WebApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.EndPoints.WebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
